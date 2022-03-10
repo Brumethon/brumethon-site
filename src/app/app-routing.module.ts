4ce060adfc23b6517280c60cf56277b7090542f2
ame <amedouillard@hotmail.fr>
2022-03-10 17:20:43 +0100

    module categories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from "./components/login/login.component";
+import {CategoriesComponent} from "./components/categories/categories.component";
 
 const routes: Routes = [
-  { path: '', component: LoginComponent},
-  { path: 'login', component: LoginComponent}
+  { path: '', component: CategoriesComponent},
+  { path: 'categories', component: CategoriesComponent}
 ];
 
 @NgModule({
