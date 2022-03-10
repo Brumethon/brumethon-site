1163c6443dc40b3334cec9434a93d74d661147bd
Nathanael TO <nathanaelto.pro@gmail.com>
2022-03-10 13:57:18 +0100

    first commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validator, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: [
+    './login.component.css',
+    '../../../assets/vendor/animate/animate.css',
+    '../../../assets/vendor/css-hamburgers/hamburgers.min.css',
+    '../../../assets/vendor/animsition/css/animsition.min.css',
+    '../../../assets/vendor/select2/select2.min.css',
+    '../../../assets/vendor/daterangepicker/daterangepicker.css',
+    '../../../assets/css/util.css',
+    '../../../assets/css/main.css'
+  ]
+})
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  async onSubmitForm() {
+    const {email, password} = this.loginForm.value;
+
+    /// TODO : required services et router
+  }
+
+}
