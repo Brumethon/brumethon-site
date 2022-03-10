34bd0abf9712292762ef8a21e31f21205f72ff5d
ame <amedouillard@hotmail.fr>
2022-03-10 19:08:20 +0100

    ajoute base url dans DefaultRepository.ts

--- src/app/repository/DefaultRepository.ts
+++ src/app/repository/DefaultRepository.ts
@@ -3,9 +3,11 @@ import {environment} from "../../environments/environment";
 export abstract class DefaultRepository{
 
   protected token: string;
+  protected baseUrl: string;
 
   protected constructor() {
-    this.token = environment.token
+    this.token = environment.ADMIN_TOKEN
+    this.baseUrl = environment.API_URL
   }
 
 }
